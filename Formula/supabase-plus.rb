da9b2356b6615d0fe637044dbbe0905efda04233
Dmndz <barbara@dsplce.co>
2025-11-07 20:32:08 +0100

    feat: update to 0.5.0

--- Formula/supabase-plus.rb
+++ Formula/supabase-plus.rb
@@ -1,11 +1,12 @@
 class SupabasePlus < Formula
   desc "An extra set of tools for managing Supabase projects going beyond the possibilities of regular Supabase CLI"
   homepage "https://github.com/dsplce-co/supabase-plus"
-  url "https://github.com/dsplce-co/supabase-plus/releases/download/0.4.13/sbp-aarch64-apple-darwin.tar.gz"
-  sha256 "9fdf2561686dbd547f756c5ce1d0643896aef91681f3cf24f42fdc4cb8778a4f"
-  version "0.4.13"
+  url "https://github.com/dsplce-co/supabase-plus/releases/download/0.5.0/sbp-aarch64-apple-darwin.tar.gz"
+  sha256 "716e0f962821ddb3ce8ab7dec19c1ce79d6ec80e3583d1a5f78e2d02bf319a35"
+  version "0.5.0"
 
   def install
     bin.install "sbp"
   end
 end
+
