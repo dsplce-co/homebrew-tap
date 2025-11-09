6608aff0beb32d8ea287d4a76b0b632b0845920a
Przemek <violenceplg7@gmail.com>
2025-11-09 14:02:36 +0100

    fix: correct the version meta field

--- Formula/supabase-plus.rb
+++ Formula/supabase-plus.rb
@@ -3,7 +3,7 @@ class SupabasePlus < Formula
   homepage "https://github.com/dsplce-co/supabase-plus"
   url "https://github.com/dsplce-co/supabase-plus/releases/download/0.6.0/sbp-aarch64-apple-darwin.tar.gz"
   sha256 "414a7da38db7c9f1de1915dc8d5edbd6b5dac330b433c84abafb3f327e4b05d1"
-  version "0.5.0"
+  version "0.6.0"
 
   def install
     bin.install "sbp"
